b852edf8744dec60cdf8e0ab591da6c32e4b8142
Saptarshi Sarkar <67330269+saptarshisarkar20@users.noreply.github.com>
2021-10-25 11:59:00 +0530

    Create shorts#6.cpp

---
+++ shorts#6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
