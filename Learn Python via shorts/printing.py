0ead49a313d75ab3cf7202975e12a2fd479518c5
Saptarshi Sarkar <67330269+saptarshisarkar20@users.noreply.github.com>
2022-01-16 23:26:45 +0530

    Add files via upload

---
+++ Learn Python via shorts/printing.py
@@ -0,0 +1,20 @@
+# THIS IS HOW TO PRINT IN PYTHON
+
+# JUST USE A 'print()' STATEMENT
+# ITS THAT MUCH EASY
+
+
+print("Hello World!")
+# just warp the string to print in double quote
+# Lets execute it
+
+print("Hello Python")
+# by default print statement executes in the next line .
+
+# if you want to print in the same line use "end=""' in the print statement
+
+print("it is the 1st line ", end="")
+print("It is the second line")
+# make sure you put a comma unless its an error
+
+# That's all for today
---
+++ Learn Python via shorts/variables.py
@@ -0,0 +1,40 @@
+# How to declare variables in python
+
+#its very easy just like algebra
+
+a = 10
+# Yes We have declared a Variable
+# lets see 
+print(a)
+# mind it that we dont warp variable in double quote while printing .
+
+#what if we warp it in "" 🤔
+
+# lets see
+x=58090
+print(x)
+print("x")
+# 🤔 what is your Opinion 
+# so when we wrapped it in "" it prints out only the thing we wrapped inside "" in the other case it prints the value of X 
+
+# lets declare flots and strings 
+
+# Integer 
+a= 290
+# Float
+b = 35.86
+# String
+c = "It is a String"
+
+print(a)
+print(b)
+print(c) 
+
+# how to print with custom dialouges 🤔
+# Lets see
+
+print("It is a Integer ", a)
+print("It is a Float ", b)
+print("It is a String", c)
+
+# thats all for today 
